d9afe2ab52b7ef4b2513d74c9a77b14b0ad31ae5
Dolapo Alli <dolapoalli@Dolapos-MBP.attlocal.net>
2022-08-01 22:53:23 -0400

    worked on backend logic, database connection

--- build/index.js
+++ build/index.js
@@ -1,34 +1,50 @@
 "use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
-const axios_1 = __importDefault(require("axios"));
-let cors = require('cors');
+const mongoose_1 = __importDefault(require("mongoose"));
+const body_parser_1 = __importDefault(require("body-parser"));
 const app = (0, express_1.default)();
-app.use(cors());
-app.get("/", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
-    yield axios_1.default.get("https://jsonplaceholder.typicode.com/posts")
-        .then(response => res.send((response.data)));
+app.use(body_parser_1.default.urlencoded({
+    extended: true
 }));
-app.get("/post/:id", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
-    const id = req.params.id;
-    const reqBody = req.body;
-    yield axios_1.default.get("https://jsonplaceholder.typicode.com/posts/" + id)
-        .then(response => res.send((response.data)));
-    //  await axios.put("https://jsonplaceholder.typicode.com/posts/" + id)
-    // .then(response => res.send(console.log(reqBody)))
-}));
-app.listen("4678", () => {
-    console.log("SERVER ON 4678");
+app.use(express_1.default.json());
+mongoose_1.default.connect("mongodb://localhost:27017/postsDB");
+const postsSchema = new mongoose_1.default.Schema({
+    title: { type: String },
+    body: { type: String }
+});
+const Post = mongoose_1.default.model("Post", postsSchema);
+app.get('/', (req, res) => {
+    Post.find(function (err, posts) {
+        if (!err) {
+            res.send(posts);
+        }
+        else {
+            res.send(err);
+            console.log(err);
+        }
+    });
+});
+app.get('/posts/:title', (req, res) => {
+    Post.findOne({ title: req.params.title }, (err, post) => {
+        res.send(post);
+    });
+});
+app.patch('/posts/:title', (req, res) => {
+    console.log(req.body);
+    Post.updateOne({ title: req.params.title }, { $set: req.body }, function (err) {
+        if (!err) {
+            res.send("Patch Success");
+        }
+        else {
+            res.send("Wahala");
+            console.log(err);
+        }
+    });
+});
+app.listen(4990, () => {
+    console.log("PORT 4990");
 });
--- src/index.ts
+++ src/index.ts
@@ -1,50 +1,60 @@
-import express, {Application, Request, Response} from "express";
-import axios from "axios";
+import express, { Application, Request, Response } from "express";
+import mongoose from "mongoose";
+import bodyParser from "body-parser";
 
+const app: Application = express();
+app.use(bodyParser.urlencoded({
+    extended: true
+}))
 
+app.use(express.json())
 
-let cors = require('cors')
+mongoose.connect("mongodb://localhost:27017/postsDB");
 
-const app:Application = express();
-
-
-
-
-app.use(cors())
-
-app.get("/", async (req : Request, res: Response) =>{
- await axios.get("https://jsonplaceholder.typicode.com/posts")
-.then(response => res.send((response.data)))
+const postsSchema = new mongoose.Schema({
+    title: {type: String},
+    body: {type: String}
 })
 
 
-app.get("/post/:id", async (req : Request, res: Response) =>{
-const id = req.params.id
-const reqBody = req.body
+const Post = mongoose.model("Post", postsSchema);
 
+app.get('/', (req: Request, res: Response) => {
+    Post.find(function (err, posts) {
+        if (!err) {
+            res.send(posts)
+        }
+        else {
+            res.send(err)
+            console.log(err)
+        }
 
-
-
-
- await axios.get("https://jsonplaceholder.typicode.com/posts/" + id)
-.then(response => res.send((response.data)))
-
-
-
-//  await axios.put("https://jsonplaceholder.typicode.com/posts/" + id)
-// .then(response => res.send(console.log(reqBody)))
-
-
-
-
-
-
+    })
 })
- 
 
+app.get('/posts/:title', (req: Request, res: Response) => {
+    Post.findOne({ title: req.params.title }, (err: any, post: any) => {
+        res.send(post)
+    })
+})
 
+app.patch('/posts/:title', (req: Request, res: Response) => {
+    console.log(req.body)
+    Post.updateOne(
+        { title: req.params.title },
+        { $set: req.body },
+        function (err: any) {
+            if (!err) {
+                res.send("Patch Success")
+            }
+            else {
+                res.send("Wahala")
+                console.log(err)
+            }
+        }
+    )
+})
 
-app.listen("4678", () : void=>{
-    console.log("SERVER ON 4678");
-    
+app.listen(4990, (): void => {
+    console.log("PORT 4990");
 })
